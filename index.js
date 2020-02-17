8fa9e98d0ea64b0d132f008870a1a783024a1b35
Fadeev Ivan <githubfadeev@gmail.com>
2020-02-17 15:34:00 +0300

    DB initial

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 async function start() {
     try {
-        await mongoose.connect('', {
+        await mongoose.connect("mongodb+srv://amdin:<password>@cluster0.jbj6b.mongodb.net/<dbname>?retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useFindAndModify: false
         });
@@ -21,3 +21,17 @@ async function start() {
 }
 
 start();
+
+
+/*
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://amdinfadeev:<password>@cluster0.jbj6b.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+*/
