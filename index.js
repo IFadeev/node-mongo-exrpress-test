eac3df71789f79c6454e61247f407ad499b52f1f
Fadeev Ivan <githubfadeev@gmail.com>
2020-02-17 19:34:00 +0300

    Added styles and first routing

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+const todoRouters = require('./routes/todos');
+
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
 
+app.engine('hbs', hbs.engine); //регистрация движка
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(todoRouters);
 
 async function start() {
     try {
-        await mongoose.connect("mongodb+srv://amdin:<password>@cluster0.jbj6b.mongodb.net/<dbname>?retryWrites=true&w=majority", {
+        await mongoose.connect("mongodb+srv://amdinfadeev:34tmQ34t233sd@cluster0.jbj6b.mongodb.net/<dbname>?retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useFindAndModify: false
         });
